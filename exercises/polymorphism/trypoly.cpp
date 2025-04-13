b0b8efc87661b35212c405c7bb09dcc48834b627
Kateryna <kataokopnaa@gmail.com>
2025-04-13 19:44:08 +0300

    Day 2 polymorphism - solved

--- exercises/polymorphism/trypoly.cpp
+++ exercises/polymorphism/trypoly.cpp
@@ -3,13 +3,18 @@
 
 int main() {
     // create a Pentagon, call its perimeter method
-
+    Pentagon penta{5.0};
+    std::cout << "Penta : perimeter = " << penta.computePerimeter() << "\n\n";
 
     // create a Hexagon, call its perimeter method
-
+    Hexagon hexa{2.0};
+    std::cout << "Hexa : perimeter = " << hexa.computePerimeter() << "\n\n";
 
     // create a Hexagon, call the perimeter method through a reference to Polygon
-
+    Hexagon hexa2{1.0};
+    RegularPolygon &poly = hexa2;
+    std::cout << "Hexa : perimeter = " << hexa2.computePerimeter() << '\n'
+              << "Hexa as Poly : perimeter = " << poly.computePerimeter() << '\n';
 
     // retry virtual method
 
